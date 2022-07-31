2ae61ab666b2d610aca45c23ff33e240cf228853
Paulo Santiago <paulosantiagojf@gmail.com>
2022-07-31 18:22:46 -0300

    Projeto concluído

---
+++ src/DevIO.Api/Configuration/LoggerConfig.cs
@@ -0,0 +1,52 @@
+﻿using DevIO.Api.Extensions;
+using Elmah.Io.Extensions.Logging;
+using HealthChecks.UI.Client;
+using Microsoft.AspNetCore.Diagnostics.HealthChecks;
+
+namespace DevIO.Api.Configuration
+{
+    public static class LoggerConfig
+    {
+        public static IServiceCollection AddLoggingConfig(this IServiceCollection services, IConfiguration configuration)
+        {
+            var apiKey = configuration["Elmah:ApiKey"]; 
+            var logId = configuration["Elmah:LogId"];
+            var heartbeatId = configuration["Elmah:HeartbeatId"];
+
+            services.AddElmahIo(o =>
+            {
+                o.ApiKey = apiKey;
+                o.LogId = new Guid(logId);
+            });
+
+            services.AddHealthChecks()
+                .AddElmahIoPublisher(options =>
+                {
+                    options.ApiKey = apiKey;
+                    options.LogId = new Guid(logId);
+                    options.HeartbeatId = heartbeatId;
+
+                })
+                .AddCheck("Produtos", new MySqlHealthCheck(configuration.GetConnectionString("DefaultConnection")))
+                .AddMySql(configuration.GetConnectionString("DefaultConnection"), name: "Database");
+            
+            services.AddHealthChecksUI()
+                .AddMySqlStorage(configuration.GetConnectionString("DefaultConnection"));
+
+            return services;
+        }
+
+        public static IApplicationBuilder UseLoggingConfiguration(this IApplicationBuilder app)
+        {
+            app.UseElmahIo();
+
+            app.UseHealthChecks("/api/hc", new HealthCheckOptions()
+            {
+                Predicate = _ => true,
+                ResponseWriter = UIResponseWriter.WriteHealthCheckUIResponse
+            });
+
+            return app;
+        }
+    }
+}
---
+++ src/DevIO.Api/Extensions/MySqlHealthCheck.cs
@@ -0,0 +1,36 @@
+﻿// using Microsoft.Data.SqlClient;
+using Microsoft.Extensions.Diagnostics.HealthChecks;
+using MySqlConnector;
+
+namespace DevIO.Api.Extensions
+{
+    public class MySqlHealthCheck : IHealthCheck
+    {
+        readonly string _connection;
+
+        public MySqlHealthCheck(string connection)
+        {
+            _connection = connection;
+        }
+
+        public async Task<HealthCheckResult> CheckHealthAsync(HealthCheckContext context, CancellationToken cancellationToken = new CancellationToken())
+        {
+            try
+            {
+                using (var connection = new MySqlConnection(_connection))
+                {
+                    await connection.OpenAsync(cancellationToken);
+
+                    var command = connection.CreateCommand();
+                    command.CommandText = "select count(id) from produtos";
+
+                    return Convert.ToInt32(await command.ExecuteScalarAsync(cancellationToken)) > 0 ? HealthCheckResult.Healthy() : HealthCheckResult.Unhealthy();
+                }
+            }
+            catch (Exception)
+            {
+                return HealthCheckResult.Unhealthy();
+            }
+        }
+    }
+}
